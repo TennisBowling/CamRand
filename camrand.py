9dc3cdee71dd6d4f17f1e71e584dd912d0f13107
TennisBowling <fastspy@Outlook.com>
2021-02-14 08:27:21 -0800

    alright lets see

--- camrand.py
+++ camrand.py
@@ -2,11 +2,13 @@ import os, hashlib
 from PIL import Image
 from decimal import Decimal
 import subprocess
-import pyximport; pyximport.install()
+from numba import jit
 
+@jit
 def hash_to_int(obj):
     return int(hashlib.sha512(repr(obj).encode('utf-8')).hexdigest(), 16)
 
+@jit(nopython=True)
 def numberToBase(n:int, b:int = -1, key:str or list = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/"):
     if(b == -1): b = len(key)
     digits = ""
@@ -19,18 +21,20 @@ class RandomImageSource:
     def __init__(self):
         self.last_random = 0
         self.last_random = self.get_seed()
-    
+
     def color_algo(self, pxs:tuple):
         return ''.join(map(lambda x: str(bin(x))[2:], pxs))
 
+    @jit
     def gray_algo(self, pxs:tuple):
         gray = int(sum(pxs) / len(pxs))
         return str(bin(gray))[2:]
-    
+
+    @jit
     def bw_algo(self, pxs:tuple):
         gray = sum(pxs) / len(pxs)
         return "1" if gray > 127.5 else "0"
-    
+
     def get_raw_int(self, algorith=None):
         if algorith == None: algorith = self.gray_algo
 
@@ -49,9 +53,11 @@ class RandomImageSource:
         self.last_random = rand
         return rand
     
+    @jit
     def get_seed(self):
         rand_int = self.last_random - self.get_raw_int(algorith=self.gray_algo)
         return rand_int if rand_int > 0 else -rand_int
+
 
     def get_random(self):
         seed = self.get_seed()
